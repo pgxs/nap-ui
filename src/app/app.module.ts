3514ac43843201df265ed38309f46d1dd67ecb9d
veily <drmfly.liw@gmail.com>
2020-03-26 22:07:44 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// DevUI部分组件依赖angular动画，需要引入animations模块
+import { DevUIModule } from 'ng-devui';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +13,14 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DevUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
